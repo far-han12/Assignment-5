d4c88b3df4ff794dab3dd390e215d6e18b1e7f7a
far-han12 <mahmudfarhan590@gmail.com>
2024-02-19 03:22:47 +0600

    js added

---
+++ scripts/index.js
@@ -0,0 +1,84 @@
+
+
+// ticket selection
+let totalprice=0
+
+let count= 0 
+const ticketSelection = document.getElementsByClassName("seat")
+
+for (let index = 0; index < ticketSelection.length; index++) {
+    const element = ticketSelection[index];
+;
+    // console.log(element);
+    element.addEventListener("click",function(){
+        element.style.backgroundColor = "#1DD100"
+        element.style.color="white"
+        count++
+if(count>4){
+    alert("You can not buy more than 4 tickets")
+    element.style.backgroundColor="#0307121A"
+    return
+}
+
+        // appending
+        const mainpricecontainer=document.getElementById("main-container")
+        const tpp= document.getElementById("seat-per-price").innerText
+        const ticketperprice=parseInt(tpp)
+const seatstatusname = document.getElementById("container-3")
+// const seatname = document.getElementById('s')
+const p3 = document.createElement("p")
+p3.innerText=this.innerText
+seatstatusname.appendChild(p3)
+mainpricecontainer.appendChild(seatstatusname)
+
+
+        const economyclass = document.getElementById("container-2")
+        const p2 = document.createElement("p")
+        p2.innerText="Economy"
+        economyclass.appendChild(p2)
+        mainpricecontainer.appendChild(economyclass)
+    
+        const priceContainer = document.getElementById("container-1")
+        const p = document.createElement("p")
+        p.innerText=ticketperprice
+        priceContainer.appendChild(p)
+        mainpricecontainer.appendChild(priceContainer)
+    totalprice+=ticketperprice
+    document.getElementById("total-price").innerText=totalprice
+document.getElementById("grand-total-price").innerText=totalprice
+ 
+
+  
+    })
+  
+
+    
+}
+
+
+const applyBtn = document.getElementById("apply-btn")
+applyBtn.addEventListener('click',function(){
+    const ci=document.getElementById("coupon-input")
+    
+    const couponInput=ci.value
+    const couponcode=couponInput.split(" ").join(" ").toUpperCase()
+  if(couponcode==="NEW15"){
+    const discountamount = totalprice*0.15
+    document.getElementById("grand-total-price").innerText=totalprice-discountamount
+    ci.classList.add("hidden")
+  }
+  
+ else if(couponcode==="COUPLE20"){
+    const discountamount = totalprice*0.20
+    document.getElementById("grand-total-price").innerText=totalprice-discountamount
+ 
+  }
+  else{
+    alert("wrong coupon code")
+  }
+})
+
+
+
+
+
